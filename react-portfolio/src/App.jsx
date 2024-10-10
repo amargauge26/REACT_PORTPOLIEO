99a42e6549229f767356a6c4bebd64a1514ad4e6
amargauge26 <amardeep.singh0755@gmail.com>
2024-10-10 23:09:17 +0530

    day 2 nab var

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,11 +1,14 @@
 
 import styles from './App.module.css'
+import {Navbar} from "./componets/Navbar/Navbar.jsx";
 
 function App() {
   
 
   return (
-    <div className={styles.App}></div>
+    <div className={styles.App}>
+      <Navbar/>
+    </div>
   )
 }
 
